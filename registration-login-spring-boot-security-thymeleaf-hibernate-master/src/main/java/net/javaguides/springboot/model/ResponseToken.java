ee4f428d0b0673070a2191a61f978761e2ea356d
Mounika13Simhadri <122076146+Mounika13Simhadri@users.noreply.github.com>
2023-04-13 11:25:54 +0530

    Add files via upload

---
+++ registration-login-spring-boot-security-thymeleaf-hibernate-master/src/main/java/net/javaguides/springboot/model/ResponseToken.java
@@ -0,0 +1,15 @@
+package net.javaguides.springboot.model;
+
+
+public class ResponseToken {
+
+	private String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
